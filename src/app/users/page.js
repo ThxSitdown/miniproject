30855e322a4565a0acc07f9ffce07ed861e7d9ac
ThxSitdown <surajakzaza15251@gmail.com>
2024-08-23 08:31:37 +0700

    .

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,14 +1,14 @@
 'use client';
 import Link from 'next/link'
-
 import { useEffect, useState } from 'react';
 
 export default function Page() {
   const [items, setItems] = useState([]);
+
   useEffect(() => {
     async function getUsers() {
       try {
-        const res = await fetch('http://localhost:3000/api/users');
+        const res = await fetch('http://localhost:3001/api/users');
         if (!res.ok) {
           console.error('Failed to fetch data');
           return;
@@ -20,51 +20,70 @@ export default function Page() {
       }
     }
  
-  getUsers()
-  const interval  = setInterval(getUsers, 1000);
-  return () => clearInterval(interval );
-}, []);
+    getUsers();
+    const interval  = setInterval(getUsers, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleDelete = async (id) => {
+    // Confirm before delete
+    const confirmDelete = window.confirm('Are you sure you want to delete this user?');
+    if (!confirmDelete) return;
+
+    try {
+      const res = await fetch(`http://localhost:3001/api/users/${id}`, {
+        method: 'DELETE',
+      });
+      if (!res.ok) {
+        console.error('Failed to delete user');
+        return;
+      }
+      // Remove deleted item from the list
+      setItems(items.filter(item => item.id !== id));
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  }
 
   return (
     <>
-
-    <br /><br /><br /><br />
-    <div className="container">
-      <div class="card">
-  <div class="card-header">
-    Users List
-  </div>
-  <div class="card-body">
-  <div className="row">
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th className='col-md-2 text-center'>#</th>
-            <th className='col-md-4'>Firstname</th>
-            <th className='col-md-4'>Lastname</th>
-            <th className='col-md-1'>Eidt</th>
-            <th className='col-md-1'>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <tr key={item.id}>
-              <td className='text-center'>{item.id}</td>
-              <td>{item.firstname}</td>
-              <td>{item.lastname}</td>
-              <td><Link href="#" className="btn btn-warning">Edit</Link></td>
-              <td><Link href="#" className="btn btn-danger">Del</Link></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    </div>
-
-    </div>
-    </div>
-    <br /><br />
-
+      <br /><br /><br /><br />
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            Users List
+          </div>
+          <div className="card-body">
+            <div className="row">
+              <table className="table table-striped table-hover">
+                <thead>
+                  <tr>
+                    <th className='col-md-2 text-center'>#</th>
+                    <th className='col-md-4'>Firstname</th>
+                    <th className='col-md-4'>Lastname</th>
+                    <th className='col-md-1'>Edit</th>
+                    <th className='col-md-1'>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item) => (
+                    <tr key={item.id}>
+                      <td className='text-center'>{item.id}</td>
+                      <td>{item.firstname}</td>
+                      <td>{item.lastname}</td>
+                      <td><Link href={`/users/edit/${item.id}`} className="btn btn-warning">Edit</Link></td>
+                      <td>
+                        <button onClick={() => handleDelete(item.id)} className="btn btn-danger">Del</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br /><br />
     </>
   );
 }
