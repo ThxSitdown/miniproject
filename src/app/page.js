008d16b0d2b7b10709290322cd90e84c5675dc72
ThxSitdown <surajakzaza15251@gmail.com>
2024-08-29 00:06:42 +0700

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import SensorDataGraph from './component/SensorDataGraph';
-import LEDtoggle from './component/LEDtoggle';
 
 export default function Home() {
   return (
     <>
       <SensorDataGraph/>
-      <LEDtoggle/>
     </>
   );
 }
