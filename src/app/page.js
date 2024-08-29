e3008effd51af4b11daac36be80487cbebdbce08
ThxSitdown <surajakzaza15251@gmail.com>
2024-08-29 18:26:14 +0700

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import SensorDataGraph from './component/SensorDataGraph';
-import LEDControl from '../components/LEDControl';
+import LEDControl from './components/LEDControl';
 
 export default function Home() {
   return (
     <>
       <SensorDataGraph/>
-      <LEDControl />
+      <LEDControl/>
     </>
   );
 }
