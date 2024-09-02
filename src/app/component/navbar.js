29ce0a18364aab501d7acb5fd06b8b19cc671ecb
ThxSitdown <surajakzaza15251@gmail.com>
2024-09-02 09:43:26 +0700

    .

--- src/app/component/navbar.js
+++ src/app/component/navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="/">Navbar</Link>
+                <Link className="navbar-brand" href="/">Bearded Dragon Climate Control Simulator</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
